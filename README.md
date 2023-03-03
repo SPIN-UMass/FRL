657ab970420f19827b0a65f5ec1d48e3f70531a7
Hamid Mozaffari <hamid@mail.cs.umass.edu>
2023-03-03 15:18:23 -0500

    update FRL

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# FRL
+# Federated Rank Learning (FRL) 
+
+This repository contains the implementation of Federated Rank Learning (FRL) algorithm, as described in our paper: Every Vote Counts: Ranking-Based Training of Federated Learning to Resist Poisoning Attacks, to appear on USENIX Security Symposium 2023. 
+
+## Getting Started
+
+### Prerequisites
+
+Please make sure you have the following dependencies installed:
+
+- PyTorch 1.13.1 
+- Numpy 1.23.5
+
+### Installation
+
+To download the repository, use the following command:
+
+git clone https://github.com/SPIN-UMass/FRL.git
+
+Then, to install the dependencies, run:
+
+pip install -r requirements.txt
+
+## Basic Test
+
+To run a simple experiment on CIFAR10, please run the following command:
+
+python experiments/001_config_CIFAR10_Conv8_FRL_1000users_noniid1.0_nomalicious.txt
+
+
+This will distribute CIFAR10 over 1000 clients in a non-iid fashion with a Dirichlet distribution parameter $\beta=1.0$. Then, a federated rank learning will be run on top of these 1000 users for 2000 global FL rounds, where 25 clients are chosen for their local update in each round.
---
+++ main.py
@@ -0,0 +1,65 @@
+import os
+from args import args
+import random
+import numpy as np
+import pathlib
+import torch
+
+import data
+from FL_train import *
+
+
+
+def main():
+    if args.seed is not None:
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        torch.cuda.manual_seed(args.seed)
+        torch.cuda.manual_seed_all(args.seed)
+        
+        
+    # Make the a directory corresponding to this run for saving results, checkpoints etc.
+    i = 0
+    while True:
+        run_base_dir = pathlib.Path(f"{args.log_dir}/FRL~try={str(i)}")
+
+        if not run_base_dir.exists():
+            os.makedirs(run_base_dir)
+            args.name = args.name + f"~try={i}"
+            break
+        i += 1
+
+    (run_base_dir / "output.txt").write_text(str(args))
+    args.run_base_dir = run_base_dir
+
+    print(f"=> Saving data in {run_base_dir}")
+    
+    
+    
+    #distribute the dataset
+    print ("dataset to use is: ", args.set)
+    print ("number of FL clients: ", args.nClients)
+    print ("non-iid degree data distribution: ", args.non_iid_degree)
+    print ("batch size is : ", args.batch_size)
+    print ("test batch size is: ", args.test_batch_size)
+    
+    data_distributer = getattr(data, args.set)()
+    tr_loaders = data_distributer.get_tr_loaders()
+    te_loader = data_distributer.get_te_loader()
+    
+    
+    args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    use_cuda = torch.cuda.is_available()
+    print ("use_cuda: ", use_cuda)
+    
+    
+    #Federated Learning
+    print ("type of conv layer: ", args.FL_type)
+    if args.FL_type == "FRL":
+        FRL_train(tr_loaders, te_loader)
+    else:
+        Standard_FL_train()
+
+   
+if __name__ == "__main__":
+    main()
